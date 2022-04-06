10611ed0db9d51ab99e11d5e3ae78e20071c40b8
Elliot Juniper <elliotjuniper@gmail.com>
2022-04-06 17:13:37 +0100

    new

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 document.addEventListener("keydown", (event) => {
-    let numDisplay = document.getElementById("numDisplay");
-    let numCode = document.getElementById("codeDisplay");
-    let numKey = document.getElementById("keyDisplay");
-    let numWhich = document.getElementById("whichDisplay");
+    let numDisplay = document.getElementById("num");
+    let numWhich = document.getElementById("which1");
+    let numKey = document.getElementById("key1");
+    let numCode = document.getElementById("code1");
 
-    let keyDown = event.key;
     let keyCode = event.code;
     let keyWord = event.which;
+    let keyDown = event.key;
 
-numDisplay.innerHTML = `<p class="pressedBtn"> ${keyDown} was pressed</p>`
-numKey.innerHTML = `<p class="pressedBtn"> ${keyCode} was pressed</p>`
-numCode.innerHTML = `<p class="pressedBtn"> ${keyWord} was pressed</p>`
-numWhich.innerHTML = `<p class="pressedBtn"> ${keyDown} was pressed</p>`
+numWhich.innerHTML = `<p class="button1"> ${keyDown} was used</p>`
+numDisplay.innerHTML = `<p class="button1"> ${keyDown} was used</p>`
+numCode.innerHTML = `<p class="button1"> ${keyWord} was used</p>`
+numKey.innerHTML = `<p class="button1"> ${keyCode} was used</p>`
 })
 
