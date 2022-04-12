0a3fc214165101113bc8cd60a4033d92e1cbd555
Elliot Juniper <elliotjuniper@gmail.com>
2022-04-12 15:34:18 +0100

    Notes

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+
+// EVENT LISTENERS 'LISTEN' FOR AN ACTION TO OCCUR AND IN THIS CASE IT IS PRESSING A KEY ON THE KEYBOARD. EACH ELEMENT CORRESPONDS TO A CERTAIN ELEMENT/ID FOUND ON THE HTML FILE.
 document.addEventListener("keydown", (event) => {
     let numDisplay = document.getElementById("num");
     let numWhich = document.getElementById("which1");
     let numKey = document.getElementById("key1");
     let numCode = document.getElementById("code1");
 
+    // .NOTATION THAT CALLS THE FUNCTION
     let keyCode = event.code;
     let keyWord = event.which;
     let keyDown = event.key;
 
+
+// INNERHTML IS USED WHEN YOU WANT TO RETURN THE HTML ITEMS PLACED WITHIN AN ELEMENT. TEMPLATE LITERALS USED FOR VARIABLES WITHIN A STRING. BY INITIALISING THESE COMMANDS, ONCE A KEY IS PRESSED ON THE KEYBOARD, IT WILL TRANSFER THE INPUT INTO THE CORRESPONDING TEXT WITHIN THE WEBPAGE. 
 numWhich.innerHTML = `<p class="button1"> ${keyDown} was used</p>`
 numDisplay.innerHTML = `<p class="button1"> ${keyDown} was used</p>`
 numCode.innerHTML = `<p class="button1"> ${keyWord} was used</p>`
